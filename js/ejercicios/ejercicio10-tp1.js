7b789948c41d85b111bbe3a83bf47915312ad57d
matias <pagosptcmatias@gmail.com>
2022-07-17 15:20:32 -0300

    agg eje 11

--- js/ejercicios/ejercicio10-tp1.js
+++ js/ejercicios/ejercicio10-tp1.js
@@ -1,1 +1,10 @@
-//10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+//10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7
+//(sólo hay que comprobar si lo es por uno de los cuatro)
+
+let numero = parseInt(prompt("ingrese un numero"));
+
+if (numero % 2 === 0) {
+  document.write(`El numero ${numero} si es divisible en 2`);
+} else {
+    document.write(`El numero ${numero} no es divible en 2`)
+}
--- js/ejercicios/ejercicio11-tp1.js
+++ js/ejercicios/ejercicio11-tp1.js
@@ -1,1 +1,13 @@
-//11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+//11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible 
+//(hay que decir todos por los que es divisible)
+let numero = parseInt(prompt("ingrese un numero"));
+
+if (numero % 2 === 0) {
+  document.write(`El numero ${numero} si es divisible en 2`);
+} else if (numero % 3 === 0){
+    document.write(`El numero ${numero} es divible en 3`)
+} else if (numero % 5 === 0){
+    document.write(`El numero ${numero}  es divible en 5`)
+}  else {
+    document.write(`El numero ${numero}  es divible en 7`)
+}
